0eff38349c30cdbd86755717716b118070448ba5
Stefan Buck <web@broud.de>
2014-03-18 23:02:39 +0100

    implement CRUD operations

--- app/services/articles.js
+++ app/services/articles.js
@@ -1,34 +1,72 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+    Article = mongoose.model('Article'),
+    _ = require('lodash');
 
 
 module.exports = {
 
-  get: function (id, query, callback) {
-    Article.load(id, function(err, article) {
-      // TODO handle error
-      callback(err, article);
-    });
-  },
+    getById: function (id, callback) {
+        Article.load(id, function (err, article) {
+            if (err) return next(err);
+            if (!article) return next(new Error('Failed to load article ' + id));
+            callback(null, article);
+        });
+    },
 
-  find: function (params, callback) {
-    Article.find().sort('-created').populate('user', 'name username').exec(function (err, articles) {
-      // TODO handle error
-      callback(err, articles);
-    });
-  },
+    find: function (params, callback) {
+        Article.find().sort('-created').populate('user', 'name username').exec(function (err, articles) {
+            if (articles === null) {
+                return callback(new Error('Articles not found'));
+            }
+            callback(err, articles);
+        });
+    },
 
-  create: function(data, params, callback) {
-    var article = new Article(data);
+    get: function (id, params, callback) {
+        this.getById(id, function (err, article) {
+            if (article === null) {
+                return callback(new Error('Article not found'));
+            }
+            callback(null, article);
+        });
+    },
 
-    // TODO assign current user
-    //article.user = data.user;
+    create: function (data, params, callback) {
+        var article = new Article(data);
 
-    article.save(function(err) {
-      // TODO handle error
-      callback(err, article);
-    });
-  }
+        // TODO assign current user
+        //article.user = data.user;
+
+        article.save(function (err) {
+            callback(err, article);
+        });
+    },
+
+    update: function (id, data, params, callback) {
+        this.getById(id, function (err, article) {
+            if (article === null) {
+                return callback(new Error('Can not update article'));
+            }
+
+            article = _.extend(article, data);
+            article.save(function (err) {
+                callback(err, article);
+            });
+        });
+    },
+
+    remove: function (id, params, callback) {
+        this.getById(id, function (err, article) {
+            if (article === null) {
+                return callback(new Error('Can not delete article'));
+            }
+
+            article.remove(function (err) {
+                callback(err, article);
+            });
+        });
+
+    }
 };
